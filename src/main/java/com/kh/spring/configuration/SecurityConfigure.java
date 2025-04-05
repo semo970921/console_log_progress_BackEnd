cbb1b6351c071980e1f9ea304019a40894928da0
SeungWon <semo970921@naver.com>
2025-04-06 02:40:39 +0900

    로그인 앞단과 연결

--- src/main/java/com/kh/spring/configuration/SecurityConfigure.java
+++ src/main/java/com/kh/spring/configuration/SecurityConfigure.java
@@ -12,6 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfigure {
@@ -39,6 +44,21 @@ public class SecurityConfigure {
   public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
     return authConfig.getAuthenticationManager();
   }
+
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+    configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+
+  }
+
 
 
 
